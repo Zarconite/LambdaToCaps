cee6d9d59aaa6a83c3ff17994a7a48ec4e2e9878
Zarconite <zarconite@gmail.com>
2018-11-21 16:08:12 +0000

    Created a method that returns it instead of a console print

--- src/lambdaToCaps/LambdaToCaps.java
+++ src/lambdaToCaps/LambdaToCaps.java
@@ -2,10 +2,16 @@ package lambdaToCaps;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LambdaToCaps {
 
 
+	public static List<String> listToUpper(List<String> stringList) {
+		
+		return stringList.stream().map(stringUpper -> stringUpper.toUpperCase()).collect(Collectors.toList());
+	}
+	
 	public static void main (String args[]) {
 
 		List<String> stringList = new ArrayList<String>(3);
@@ -13,7 +19,7 @@ public class LambdaToCaps {
 		stringList.add("It's");
 		stringList.add("Dale");
 		
-		stringList.stream().map(stringUpper -> stringUpper.toUpperCase()).forEach(System.out::println);
+		listToUpper(stringList);
 		
 	}
 	
